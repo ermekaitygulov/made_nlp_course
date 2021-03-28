0cd489db77762e64b490b1d66ffecc1420428dae
ermekaitygulov <ermek.imankulov@gmail.com>
2021-03-28 13:45:20 +0300

    clean transformers; fix lr_scheduler usage

--- homeworks/homework01_word_vectors/utils.py
+++ homeworks/homework01_word_vectors/utils.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 import numpy as np
 from matplotlib import pyplot as plt
 from collections import Counter, deque
@@ -19,11 +20,18 @@ def plot_train_process(train_loss, val_loss, train_accuracy, val_accuracy, title
 
 
 class Splitter:
+    """Class for splitting text into ngrams."""
     def __init__(self, ngrams=(1,)):
         self.ngrams = ngrams
 
     @staticmethod
     def get_ngrams(text, n=1):
+        """Split text into sequences of length n
+
+        :param text: text to split
+        :param n: length of sequences
+        :return: Counter
+        """
         window = deque(maxlen=n)
         ngram_list = list()
         for word in text.split():
@@ -34,13 +42,29 @@ class Splitter:
         return ngram_counter
 
     def split_text(self, text):
+        """Split text into sequences of length in self.ngrams
+
+        :param text: text to split
+        :return: Counter
+        """
         ngram_list = Counter()
         for n in self.ngrams:
             ngram_list.update(self.get_ngrams(text, n))
         return ngram_list
 
 
-class BagOfWords:
+class TextTransformer(ABC):
+    """TextTransformer interface"""
+    @abstractmethod
+    def transform_text(self, text):
+        raise NotImplementedError
+
+    def transform_dataset(self, dataset):
+        return np.stack(list(map(self.transform_text, dataset)))
+
+
+class BagOfWords(TextTransformer):
+    """Class to transform text into vector of words frequencies"""
     def __init__(self, dataset, max_feat=None, ngrams=(1,)):
         self.splitter = Splitter(ngrams)
         self.word_counter = Counter()
@@ -49,17 +73,18 @@ class BagOfWords:
         self.vocabulary = None
         self.set_max_feat(max_feat)
 
-    def set_max_feat(self, max_feat=None):
-        self.vocabulary = self.word_counter.most_common(max_feat)
-        self.vocabulary = [word for word, _ in self.vocabulary]
-
-    def transform(self, text):
+    def transform_text(self, text):
         text_counter = Counter(text.split())
         freq_feature = [text_counter[word] for word in self.vocabulary]
         return np.array(freq_feature, 'float32')
 
+    def set_max_feat(self, max_feat=None):
+        self.vocabulary = self.word_counter.most_common(max_feat)
+        self.vocabulary = [word for word, _ in self.vocabulary]
 
-class TFiDF:
+
+class TFiDF(TextTransformer):
+    """Class to transform text via TFiDF"""
     def __init__(self, dataset, alpha, max_feat=None, ngrams=(1,)):
         self.splitter = Splitter(ngrams)
         self.dataset_length = len(dataset)
@@ -69,12 +94,18 @@ class TFiDF:
         self.idf = None
         self.set_max_feat(max_feat)
 
-    def transform(self, text):
+    def transform_text(self, text):
         text_counter = Counter(self.splitter.split_text(text))
         tf = [text_counter[item] for item in self.vocabulary]
         tf = np.array(tf)
         tfidf = tf * self.idf
         return tfidf.astype('float32')
+
+    def set_max_feat(self, max_feat=None):
+        top_df_item_list = self.df_counter.most_common(max_feat)
+        self.vocabulary = [item for item, _ in top_df_item_list]
+        self.idf = np.array([freq for _, freq in top_df_item_list]) + self.alpha
+        self.idf = np.log(self.dataset_length) - np.log(self.idf)
 
     def compute_df(self, dataset):
         split_dataset = np.vectorize(lambda x: self.splitter.split_text(x))(dataset)
@@ -85,33 +116,29 @@ class TFiDF:
               for item in vocabulary}
         return Counter(df)
 
-    def set_max_feat(self, max_feat=None):
-        top_df_item_list = self.df_counter.most_common(max_feat)
-        self.vocabulary = [item for item, _ in top_df_item_list]
-        self.idf = np.array([freq for _, freq in top_df_item_list]) + self.alpha
-        self.idf = np.log(self.dataset_length) - np.log(self.idf)
 
-
-class TextVectorizer:
-    def __init__(self, word_vectorizer, tokenizer, text_maxlen):
-        self.word_vectorizer = word_vectorizer
+class Word2Vec(TextTransformer):
+    """Class to transform text with use of word2vec model"""
+    def __init__(self, word2vec_model, tokenizer, text_maxlen):
+        self.word2vec_model = word2vec_model
         self.tokenizer = tokenizer
         self.text_maxlen = text_maxlen
 
-    def vectorize_text(self, text):
-        vector = []
+    def transform_text(self, text):
+        text_vector = []
         for word in self.tokenizer.tokenize(text):
-            if word in self.word_vectorizer.key_to_index:
-                vector.append(self.word_vectorizer[word])
-        if len(vector) < self.text_maxlen:
-            pad_size = self.text_maxlen - len(vector)
-            vector.extend([np.zeros(self.word_vectorizer.vector_size) for _ in range(pad_size)])
-        elif len(vector) > self.text_maxlen:
-            vector = vector[:self.text_maxlen]
-        vector = np.stack(vector, axis=1)
-        return vector
+            if word in self.word2vec_model.key_to_index:
+                word_vector = self.word2vec_model[word]
+            else:
+                word_vector = np.zeros(self.word2vec_model.vector_size)
+            text_vector.append(word_vector)
+        if len(text_vector) < self.text_maxlen:
+            pad_size = self.text_maxlen - len(text_vector)
+            text_vector.extend([np.zeros(self.word2vec_model.vector_size) for _ in range(pad_size)])
+        elif len(text_vector) > self.text_maxlen:
+            text_vector = text_vector[:self.text_maxlen]
+        text_vector = np.stack(text_vector, axis=1)
+        return text_vector
 
-    def transform(self, dataset):
-        text_tensor = np.vectorize(self.vectorize_text, otypes=[np.ndarray])(dataset)
-        text_tensor = np.array(text_tensor.tolist())
-        return text_tensor
+    def set_text_maxlen(self, text_maxlen):
+        self.text_maxlen = text_maxlen
